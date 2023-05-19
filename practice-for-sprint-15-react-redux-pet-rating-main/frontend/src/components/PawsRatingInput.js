04f1121ce4b2a2656d42d8c52be25ed3e77b8a4b
rokasj <rokas@jerio.me>
2023-05-19 15:48:35 -0400

    Finish disabling input

--- practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
+++ practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
@@ -1,12 +1,49 @@
-const PawsRatingInput = ({ rating, disabled, onChange }) => {
+import { useState } from 'react';
 
+const PawsRatingInput = ({ rating, disabled, onChange }) => {
+  const [activeRating, setActiveRating] = useState(rating);
+
+const handleEnter = (e) => {
+  console.log("entered")
+  if (!disabled) {
+    let pawPos = e.currentTarget.dataset.index
+    console.log(pawPos)
+    setActiveRating(pawPos);
+  }
+}
+
+const handleLeave = (e) => {
+  if (!disabled) {
+    setActiveRating(rating);
+  }
+}
+
+  const divPaws = () => {
+    let paws = [];
+    for (let i = 1; i < 6; i++) {
+      paws.push(
+        <div key={i} data-index={i} className={i <= activeRating ? "filled" : "empty"} onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
+          <i className="fa fa-paw"></i>
+        </div>
+      )
+    }
+
+    return paws;
+  }
+  
   return (
-    <input
-      type="number"
-      disabled={disabled}
-      value={rating}
-      onChange={onChange}
-    />
+    <>
+      <input
+        type="number"
+        disabled={disabled}
+        value={rating}
+        onChange={onChange}
+        />
+
+      <div className="rating-input">
+        {divPaws()}
+      </div>
+    </>
   );
 };
 
